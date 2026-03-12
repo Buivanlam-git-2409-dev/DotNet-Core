a31a719621ab46cc884a090e0c6eeae74264e6e1
buivanlam <12345678+Buivanlam-git-2409-dev@users.noreply.github.com>
2026-03-12 11:04:29 +0700

    day 2

---
+++ myprj/Program.cs
@@ -0,0 +1,10 @@
+﻿IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+// Outputs:
+// stores/totals.txt
+// stores/sales.json
